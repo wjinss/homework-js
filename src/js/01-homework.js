9921b40d8ebaf29c244924578b20690c21c87b3e
wjinss <aastu4343@gmail.com>
2025-06-13 19:15:07 +0900

    [homework] 함수 작성 과제

--- src/js/01-homework.js
+++ src/js/01-homework.js
@@ -29,3 +29,71 @@ let userGrade = "VVIP";
 
 // "버튼이 클릭되었는지 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
 let isClicked = false;
+
+// 2. 함수 작성 과제
+
+// 2-1 인사말 메시지
+// 사용자로부터 이름을 입력받아 인사말을 출력하는 함수를 작성합니다.
+function greetUser(username) {
+  return `안녕하세요! ${username}님. 좋은 하루되세요!`;
+}
+console.log(greetUser("정우진"));
+
+// 2-2 2. 원가 계산
+// 판매가를 입력 받아 원가를 계산하는 함수를 작성합니다.
+// 판매가(세후 금액)에서 원가(세전 금액)를 계산하려면 판매가를 "100% + 세금 비율"로 나눠야 합니다. 해당 품목의 세금 비율은 3.3%입니다.
+const calculateOriginalPrice = function (priceWithTax) {
+  const TAX_RATE = 0.033;
+  return `${parseInt(priceWithTax / (1 + TAX_RATE))}원`;
+};
+console.log(calculateOriginalPrice(10000));
+
+// 2-3 주류 판매 가능 여부
+// 신분증의 나이를 확인해 주류 구매 가능 여부를 반환하는 함수를 작성합니다.
+// 19세 이상 주류 구매가 가능합니다.
+const canSellAlcohol = registrationCard => registrationCard.age >= 19 ? true : false;
+
+const registrationCard = {
+  name: '정우진',
+  age: 29,
+  gender: 'male',
+}
+console.log(canSellAlcohol(registrationCard));
+
+// 2-4 할인가 계산
+// 판매가와 할인 비율(%)을 입력 받아, 할인가를 반환하는 함수를 작성합니다.
+// [예] 판매가가 18,700원이고, 할인율이 20%인 경우 계산된 할인가는 14,960원입니다.
+
+const getDiscountedPrice = (originalPrice, discountPercent) =>{
+  const DISCOUNTED_RATE = (100 - discountPercent) / 100
+  return `${originalPrice * DISCOUNTED_RATE}원`;
+}
+
+console.log(getDiscountedPrice(18700, 20));
+
+// 2-5 등급 판단
+// 점수를 전달받아 점수, 등급과 설명을 포함한 객체를 반환하는 함수를 작성합니다.
+const testGrade = (score) => {
+  let grade = "";
+  let description = "";
+
+  if (score >= 90) {
+    grade = "A";
+    description = "매우 우수";
+  } else if (score >= 80) {
+    grade = "B";
+    description = "우수";
+  } else if (score >= 70) {
+    grade = "C";
+    description = "보통";
+  } else if (score >= 60) {
+    grade = "D";
+    description = "미달, 통과 기준 근접";
+  } else {
+    grade = "F";
+    description = "낙제";
+  }
+
+  return { score: score, grade: grade, description: description };
+};
+console.log(testGrade(87));
