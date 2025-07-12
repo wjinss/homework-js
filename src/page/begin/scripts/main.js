457b2cbff359689be11ee39fbb23c99a216861b3
wjinss <aastu4343@gmail.com>
2025-07-12 16:47:07 +0900

    feat : 5주차 과제 계산기1 초기 설정

---
+++ src/page/begin/scripts/main.js
@@ -0,0 +1,28 @@
+// (() => {
+//   const calculator = document.getElementById("calculator__container");
+
+//   const diplay = calculator.querySelector(".display");
+//   const stepsDispaly = calculator.querySelector(".steps");
+//   const butons = [...calculator.querySelectorAll("button")];
+
+//   // 각 상태별 초기 값 설정
+//   const data = {
+//     previousValue: "",
+//     currentValue: "",
+//     operator: undefined,
+//     pressedResult: false,
+//   };
+
+//   // 버튼들 순회
+//   butons.forEach((button) => {
+//     // 버튼 클릭 이벤트
+//     button.addEventListener("click", ({ target }) => {
+//       console.log("dsd");
+
+//       // 리셋 버튼일 경우
+//       // if (target.getAttribute("data-button-type") === "equal") {
+//       //   console.log("계산");
+//       // }
+//     });
+//   });
+// })();
