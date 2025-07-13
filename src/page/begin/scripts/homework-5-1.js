28833dd7be5752a6bd7758f6ee6e4457aa604f16
wjinss <aastu4343@gmail.com>
2025-07-14 01:45:18 +0900

    feat : 계산기 ver.1 구현

--- src/page/begin/scripts/homework-5-1.js
+++ src/page/begin/scripts/homework-5-1.js
@@ -4,9 +4,6 @@
   const calculator = document.querySelector(".calculator__container");
   const diplay = calculator.querySelector(".display");
   const stepsDispaly = calculator.querySelector(".steps");
-  const buttonsContainer = calculator.querySelector(".calculator__keys");
-  // 버튼만 호출
-  const buttons = calculator.querySelectorAll("button");
 
   //연산자 버튼 반환
   const operatorButtons = calculator.querySelectorAll(
@@ -15,8 +12,8 @@
 
   // console.log(currentOperator);
 
-  let num1 = 0;
-  let num2 = 0;
+  let num1;
+  let num2;
   let operator = null;
 
   calculator.addEventListener("click", (e) => {
@@ -30,26 +27,47 @@
 
     const { buttonType, key } = targetBtn.dataset;
     let result = diplay.textContent;
-    let buttonValue = 0;
+    let buttonValue;
 
     if (buttonType === "number") {
       if (!currentOperator) {
-        buttonValue = parseInt(result === "0" ? key : result + key);
+        buttonValue = result === "0" ? key : result + key;
         diplay.textContent = buttonValue;
         num1 = buttonValue;
         console.log(`num1 = ${num1}`);
       } else {
-        buttonValue = parseInt(result === "0" ? key : result + key);
+        buttonValue = result === "0" ? key : result + key;
         diplay.textContent = buttonValue;
         num2 = buttonValue;
         console.log(`num2 = ${num2}`);
       }
     }
+    if (buttonType === "decimal") {
+      diplay.textContent = `${diplay.textContent}.`;
+    }
     if (buttonType === "operator") {
       operatorButtons.forEach((btn) => btn.classList.remove(PRESSED_CLASSNAME));
       targetBtn.classList.add(PRESSED_CLASSNAME);
       operator = key;
-
+      if (num1 !== undefined && num2 !== undefined) {
+        switch (operator) {
+          case "+":
+            num1 = Number(num1) + Number(num2);
+            break;
+          case "-":
+            num1 = Number(num1) - Number(num2);
+            break;
+          case "*":
+            num1 = Number(num1) * Number(num2);
+            break;
+          case "/":
+            num1 = Number(num1) / Number(num2);
+            break;
+          default:
+            break;
+        }
+      }
+      result = 0;
       // diplay.textContent = `${num1} ${operator}`;
       diplay.textContent = `0`;
       console.log(operator);
@@ -58,28 +76,34 @@
       targetBtn.classList.remove(PRESSED_CLASSNAME);
       switch (operator) {
         case "+":
-          result = num1 + num2;
+          result = Number(num1) + Number(num2);
           break;
         case "-":
-          result = num1 - num2;
+          result = Number(num1) - Number(num2);
           break;
         case "*":
-          result = num1 * num2;
+          result = Number(num1) * Number(num2);
           break;
         case "/":
-          result = num1 / num2;
+          result = Number(num1) / Number(num2);
           break;
         default:
           break;
       }
       console.log(result);
-      diplay.textContent = result;
+
+      // 0으로 나눴을 때
+      if (result === Infinity) {
+        diplay.textContent = `∞`;
+      } else {
+        diplay.textContent = result;
+      }
     }
 
     if (buttonType === "clear") {
       result = "0";
       diplay.textContent = 0;
-      targetBtn.classList.remove(PRESSED_CLASSNAME);
+      operatorButtons.forEach((btn) => btn.classList.remove(PRESSED_CLASSNAME));
       num1 = 0;
       num2 = 0;
     }
